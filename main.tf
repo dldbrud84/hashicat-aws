84b872a9827a204326a5271ee2e241d2f62aa676
dldbrud84 <dldbrud84@naver.com>
2022-05-18 08:21:12 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -128,8 +128,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     # Name = "${var.prefix}-hashicat-instance"
-    Name = "Department"
-    values ="devops"
+    Department = "devops"
+    Billable ="true"
   }
 }
 
